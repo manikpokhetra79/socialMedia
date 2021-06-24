d0837593e12dad4b9235baafb3c849874f790614
Manik Gupta <manikpokhetra@gmail.com>
2021-06-24 14:11:54 +0530

    setup and created ejs views layouts and partials

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require('express');
-const port = 8000;
+const port = 8000;;
+const expressLayouts = require('express-ejs-layouts');
 const app = express();
+//setup express ejs layout
+app.use(expressLayouts);
+// setup ejs
+app.set('view engine' , 'ejs');
+app.set('views', './views');
+//router
+app.use('/',require('./routes/index'));
 app.listen(port,function(err){
     if(err){
         console.log("Error while connecting to Express Server");
