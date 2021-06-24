6aa65c55fa8aa21986c328510092c49a959efd4f
Manik Gupta <manikpokhetra@gmail.com>
2021-06-24 13:53:29 +0530

    created directory structure

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 const express = require('express');
+const port = 8000;
+const app = express();
+app.listen(port,function(err){
+    if(err){
+        console.log("Error while connecting to Express Server");
+        return;
+    }
+    console.log(`Succesfully connected to Express server at port : ${port}`);
+});
