eacd3d75ab531cfece8cb5ec04429dac4e05f84d
Manik Gupta <manikpokhetra@gmail.com>
2021-06-24 14:37:54 +0530

    installed and setup scss

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 const express = require('express');
 const port = 8000;;
 const expressLayouts = require('express-ejs-layouts');
+const sassMiddleware = require('node-sass-middleware');
 const app = express();
+
+// scss setup
+app.use(sassMiddleware({
+    src :'./assets/scss',
+    dest : './assets/css',
+    debug : true,
+    outputStyle : 'extended',
+    prefix : '/css'
+}));
 //setup express ejs layout
 app.use(expressLayouts);
+// use static files
+app.use(express.static('./assets'));
 // setup ejs
 app.set('view engine' , 'ejs');
 app.set('views', './views');
 //router
 app.use('/',require('./routes/index'));
+
+
 app.listen(port,function(err){
     if(err){
         console.log("Error while connecting to Express Server");
