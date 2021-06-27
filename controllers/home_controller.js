c71806b0d67632d8655cf2df7d0be16bfccb5860
Manik Gupta <manikpokhetra@gmail.com>
2021-06-27 23:07:05 +0530

    updated scss for homepage

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -14,7 +14,7 @@ module.exports.home = function(req,res){
 
         User.find({},function(err,users){
             return res.render('home',{
-                title : "Codeial | Home",
+                title : "MySocial | Home",
                 posts : posts,
                 all_users : users
         });
