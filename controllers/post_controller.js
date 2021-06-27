db5597a71ba3e66538ee5a84f0a19d1e911a3c27
Manik Gupta <manikpokhetra@gmail.com>
2021-06-27 20:41:23 +0530

    delete posts feature complete

--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -1,5 +1,5 @@
 const Post = require('../models/posts');
-
+const Comment = require('../models/comments');
 module.exports.create = function(req,res){
 
     Post.create({
@@ -12,5 +12,21 @@ module.exports.create = function(req,res){
         }
         console.log("post successfully created");
         return res.redirect('back');
+    });
+}
+
+module.exports.destroy = function(req,res){
+    Post.findById(req.params.id,function(err,post){
+        //when we are comparing two ids, mongoose provides a direct way by using .id
+        //.id means converting object id to string
+        if(post.user == req.user.id){
+            post.remove();
+            Comment.deleteMany({post : req.params.id},function(err){
+
+                return res.redirect('back');
+            });
+        }else{
+            return res.redirect('back');
+        }
     });
 }
